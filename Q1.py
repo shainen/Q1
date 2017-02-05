94eac02f30f29de1abdd44e38c5a728c8967cb91
Shainen Davidson <shainen@gmail.com>
2017-02-05 16:48:09 -0500

    all

--- Q1.py
+++ Q1.py
@@ -1,13 +1,23 @@
 import random
 import numpy as np
 
-FLIPS = 10
 SAMPLES = 100000
-q2UPBOUND = 6
-q3UPBOUND = 6
-q3LOWBOUND = 5
-q4UPBOUND = 5
-q4NUMHEADS = 5
+
+FLIPS1 = 10
+FLIPS2 = 500
+FLIPS3 = 200
+
+q2UPBOUND1 = 6
+q3LOWBOUND1 = 5
+
+q4UPBOUND1 = 5
+q4NUMHEADS1 = 5
+
+q2UPBOUND2 = 250
+q3LOWBOUND2 = 5
+
+q4UPBOUND3 = 100
+q4NUMHEADS3 = 100
 
 def coins(flips):
     return [1 if random.random()<0.6 else 0 for _ in range(flips)]
@@ -22,59 +32,40 @@ def splits(coins):
 def stats(coins):
     return (splits(coins),np.sum(coins))
 
-def just_sp(stat):
-    return list(zip(*stat))[0]
+totflips1 = 0
+q2num1=0
+q3den1=0
+q4num1=0
 
-def quest1(sp):
-    return np.mean(sp)
-
-def quest2(sp):
-    num = 0
-    for fl in sp:
-        if fl > q2UPBOUND:
-            num+=1
-    return num
-
-def quest3num(sp):
-    num = 0
-    for fl in sp:
-        if fl > q3UPBOUND:
-            num+=1
-    return num
-
-def quest3den(sp):
-    den = 0
-    for fl in sp:
-        if fl > q3LOWBOUND:
-            den+=1
-    return den
-
-def quest4(st):
-    num = 0
-    for fl, he in st:
-        if fl > q4UPBOUND and he > q4NUMHEADS:
-            num+=1
-    return num
-
-def allquestsion(st):
-    justflips = just_sp(st)
-    return (quest1(justflips),quest2(justflips),quest3num(justflips),quest3den(justflips),quest4(st))
-
-totflips = 0
-q2num=0
-q3den=0
-q4num=0
 for _ in range(SAMPLES):
-    groups, heads = stats(coins(FLIPS))
-    totflips += groups
-    if groups > q2UPBOUND:
-        q2num+=1
-    if groups > q3LOWBOUND:
-        q3den+=1
-    if groups > q4UPBOUND and heads > q4NUMHEADS:
-        q4num+=1
+    groups, heads = stats(coins(FLIPS1))
+    totflips1 += groups
+    if groups > q2UPBOUND1:
+        q2num1+=1
+    if groups > q3LOWBOUND1:
+        q3den1+=1
+    if groups > q4UPBOUND1 and heads > q4NUMHEADS1:
+        q4num1+=1
 
-ans = [totflips,q2num,q3den,q4num,SAMPLES]
+totflips2 = 0
+q2num2=0
+q3den2=0
+
+for _ in range(SAMPLES):
+    groups = splits(coins(FLIPS2))
+    totflips2 += groups
+    if groups > q2UPBOUND2:
+        q2num2+=1
+    if groups > q3LOWBOUND2:
+        q3den2+=1
+
+q4num3=0
+for _ in range(SAMPLES):
+    groups, heads = stats(coins(FLIPS3))
+    if groups > q4UPBOUND3 and heads > q4NUMHEADS3:
+        q4num3+=1
+
+ans = [totflips1,q2num1,q3den1,q4num1,totflips2,q2num2,q3den2,q4num3,SAMPLES]
         
-np.savetxt("f"+str(FLIPS)+"s"+str(SAMPLES)+".dat",ans)
+np.savetxt("s"+str(SAMPLES)+".dat",ans)
 
